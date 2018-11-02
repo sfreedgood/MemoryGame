550d93b822b0c3c2e3fd281af5e65d10361a0bbd
Sam Freedgood <sam.freedgood@gmail.com>
2018-11-02 13:31:59 -0400

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+
+function checkMatch(){
+    if (cardsInPlay.length === 2){
+        if (cardsInPlay[0] === cardsInPlay[1]){
+            alert("You found a match!");
+        }else{
+            alert("Sorry, try again")
+        }
+    } 
+}
+function flipCard(cardId){
+    console.log("User flipped: " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkMatch();
+}
+
+flipCard(0);
+flipCard(2);
+/*
 var cardOne = cards[0];
 var cardTwo = cards[2];
 var cardThree = cards[1];
@@ -16,3 +35,4 @@ if (cardsInPlay.length === 2){
         alert("Sorry, try again")
     }
 }
+*/
