4bb4042dc0f82ab247ae13fc2e08d6b6045a2706
Sam Freedgood <sam.freedgood@gmail.com>
2018-11-02 23:24:39 -0400

    first working version

--- js/main.js
+++ js/main.js
@@ -2,60 +2,59 @@ var cards = [
     {
         rank: "queen",
         suit: "diamonds",
-        cardImg: "images/queen-of-diamonds.png"
+        cardImg: "images/images/queen-of-diamonds.png"
     },
     {
         rank: "queen",
         suit: "hearts",
-        cardImg: "images/queen-of-hearts.png"
+        cardImg: "images/images/queen-of-hearts.png"
     },
     {
         rank: "king",
         suit: "diamonds",
-        cardImg: "images/king-of-diamonds.png"
+        cardImg: "images/images/king-of-diamonds.png"
     },
     {
         rank: "king",
         suit: "hearts",
-        cardImg: "images/king-of-hearts.png"
+        cardImg: "images/images/king-of-hearts.png"
     },
 ];
 var cardsInPlay = [];
 
-function checkMatch(){
-    if (cardsInPlay.length === 2){
-        if (cardsInPlay[0] === cardsInPlay[1]){
-            alert("You found a match!");
-        }else{
-            alert("Sorry, try again")
-        }
-    } 
-}
-function flipCard(cardId){
-    console.log("User flipped: " + cards[cardId].rank);
+function flipCard(){
+    var cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImg);
-    console.log(cards[cardId].suit);
-    checkMatch();
+    this.setAttribute("src", (cards[cardId].cardImg));
+    if (cardsInPlay.length === 2){
+        checkMatch();
+    }
 }
 
-flipCard(0);
-flipCard(2);
-/*
-var cardOne = cards[0];
-var cardTwo = cards[2];
-var cardThree = cards[1];
-var cardFour = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardFour);
-console.log("User flipped ", cardsInPlay[0]);
-console.log("User flipped ", cardsInPlay[1]);
+function createBoard(){
+    for (var i = 0; i < cards.length; i++){
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById("gameBoard").appendChild(cardElement);
+    }
+}
 
-if (cardsInPlay.length === 2){
-    if (cardsInPlay[0] === cardsInPlay[1]){
+function checkMatch(cardId){
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
     }else{
         alert("Sorry, try again")
-    }
+    } 
 }
+
+createBoard();
+
+/*
+Future Features:
+-reset button
+-score counter
+-card position randomizer
+-more cards
 */
